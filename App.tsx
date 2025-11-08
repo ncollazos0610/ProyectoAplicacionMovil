e2129f7897320c714cde637275bc167b52c57155
Paula <m.zuniga@unimayor.edu.co>
2025-11-08 11:25:35 -0500

    componente LikeButton

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import LikeButton from './src/components/LikeButton';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Aquí irán otros componentes como el menú o el botón de login */}
+      <LikeButton />
     </View>
   );
 }
@@ -14,7 +16,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
