1d4021f618c0f1dd7ac6327ab5e377432b6c9ea7
NICOLAS <ncollazos@unimayor.edu.co>
2025-11-13 13:57:37 -0500

    añadir like button

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
-import React from "react";
 import BotonInicio from "./src/components/BotonInicio";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import LikeButton from "./src/components/LikeButton";
 
 export default function App() {
   const handleLogin = () => {
@@ -7,10 +9,24 @@ export default function App() {
   };
 
   return (
-    <BotonInicio
-      titulo="Iniciar sesion"
-      onPress={handleLogin}
-      color="#007BFF" 
-    />
+    <>
+      <View style={styles.container}>
+        <BotonInicio
+          titulo="Iniciar sesion"
+          onPress={handleLogin}
+          color="#007BFF"
+        />
+
+        {/* Aquí irán otros componentes como el menú o el botón de login */}
+        <LikeButton />
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
