a234076ec0264cca2bfe38d9eb273778042f7453
jdiazt-troyano <jdiazt@unimayor.edu.co>
2025-11-14 18:40:51 -0500

    interfaz de inicio

--- App.tsx
+++ App.tsx
@@ -1,27 +1,66 @@
-import BotonInicio from "./src/components/BotonInicio";
 import React from "react";
 import { View, StyleSheet } from "react-native";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import type { NativeStackScreenProps } from "@react-navigation/native-stack";
+
+import BotonInicio from "./src/components/BotonInicio";
 import LikeButton from "./src/components/LikeButton";
 import Menu from "./src/components/Menu";
+import LoginScreen from "./src/screens/LoginScreen";
 
-export default function App() {
+// Tipos para navegación
+type RootStackParamList = {
+  Inicio: undefined;
+  Login: undefined;
+};
+
+type Props = NativeStackScreenProps<RootStackParamList, "Inicio">;
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+
+function PantallaPrincipal({ navigation }: Props) {
   const handleLogin = () => {
-    console.log("Sesion iniciada ");
+    navigation.navigate("Login");
   };
 
   return (
-    <>
-      <View style={[styles.container, {flex:1}]}>
-        <Menu />
-        <BotonInicio
-          titulo="Iniciar sesion"
-          onPress={handleLogin}
-          color="#007BFF"
+    <View style={[styles.container, { flex: 1 }]}>
+      <Menu />
+
+      <BotonInicio
+        titulo="Iniciar sesión"
+        onPress={handleLogin}
+        color="#007BFF"
+      />
+
+      <LikeButton />
+    </View>
+  );
+}
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Inicio">
+
+        <Stack.Screen
+          name="Inicio"
+          component={PantallaPrincipal}
+          options={{ headerShown: false }}
         />
-        
-        <LikeButton />
-      </View>
-    </>
+
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ title: "Iniciar Sesión" }}
+        />
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -30,13 +69,3 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
-
-
-{/* 
-    <View style={{ flex: 1 }}>
-      <MobileMenu />
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text style={{ color: "white" }}>Welcome to the app</Text>
-      </View>
-    </View>
- */}
